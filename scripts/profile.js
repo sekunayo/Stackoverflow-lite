d44306628203645ea08d0f5fd856f062233fd6cb
grace <graceokereke22@gmail.com>
2020-09-15 06:48:44 +1400

    changes

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,18 +1,35 @@
-
 var mode = document.getElementById("container");
 var profile = document.getElementById("open");
 var closer = document.getElementById("closer");
-profile.addEventListener("click", function(){
-      mode.style.display = "block";
-      profile.style.display="none";
+var first = document.getElementById("first-section");
+var expand = document.getElementById("effect");
+var second = document.getElementById("second-section");
+profile.addEventListener("click", function () {
+    mode.style.display = "block";
+    profile.style.display = "none";
 })
-closer.addEventListener("click", function(){
-    mode.style.display="none";
-    profile.style.display="block"
+closer.addEventListener("click", function () {
+    mode.style.display = "none";
+    profile.style.display = "block"
+
 })
 
-window.onclick = function(event){
+window.onclick = function (event) {
     if (event.target == mode) {
         mode.style.display = "none";
+    }
 }
+if (screen.width <= 767.98) {
+    profile.addEventListener("click", function () {
+        first.style.width = "70%";
+        expand.style.width = "50%";
+        second.style.width = "70%";
+
+    })
+    closer.addEventListener("click", function () {
+        first.style.width = "100%";
+        expand.style.width = "70%";
+        second.style.width = "100%";
+
+    })
 }
